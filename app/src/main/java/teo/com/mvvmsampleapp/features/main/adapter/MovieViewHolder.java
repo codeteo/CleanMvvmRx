a81287d9d8a3c4b3e8b68cc18274cbbbcfc407d2
teo <ramones.teo@gmail.com>
2017-09-11 19:26:56 +0300

    Uses Palette to extract colors

--- app/src/main/java/teo/com/mvvmsampleapp/features/main/adapter/MovieViewHolder.java
+++ app/src/main/java/teo/com/mvvmsampleapp/features/main/adapter/MovieViewHolder.java
@@ -1,11 +1,16 @@
 package teo.com.mvvmsampleapp.features.main.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v7.graphics.Palette;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,13 +20,16 @@ import teo.com.mvvmsampleapp.R;
 /**
  * View holder for the movie item.
  */
-
+@SuppressWarnings("FieldCanBeLocal")
 public class MovieViewHolder extends BaseViewHolder<MovieViewModel> {
 
     @BindView(R.id.tv_movie_title) TextView tvTitle;
     @BindView(R.id.iv_movie_image) ImageView ivPoster;
 
     private final Context context;
+
+    // Needs a strong reference so it won't be garbage collected
+    private Target target;
 
     MovieViewHolder(View itemView) {
         super(itemView);
@@ -34,12 +42,41 @@ public class MovieViewHolder extends BaseViewHolder<MovieViewModel> {
     public void setViewModel(MovieViewModel viewModel) {
         tvTitle.setText(viewModel.getTitle());
 
+        target = getTarget();
+
         Picasso.with(context)
                 .load(Constants.appendImageUrl(viewModel.getImageUrl()))
-                .fit()
-                .centerCrop()
-                .into(ivPoster);
+                .into(target);
 
+    }
+
+    private Target getTarget() {
+        return new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                ivPoster.setImageBitmap(bitmap);
+
+                Palette.from(bitmap)
+                        .generate(palette -> {
+                            Palette.Swatch swatch = palette.getVibrantSwatch();
+                            int textColor = Color.WHITE;
+                            if (swatch != null) {
+                                textColor = swatch.getRgb();
+                            }
+                            tvTitle.setTextColor(textColor);
+                        });
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        };
     }
 
 }
